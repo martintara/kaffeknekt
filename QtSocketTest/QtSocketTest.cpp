407d7f7e7fb8a7bcb51992cc9e81e4660afedfda
Martin Taraldstad <martin.taraldstad@gmail.com>
2025-05-07 14:40:44 +0200

    Didrik: Added QtSocketTest.cpp

---
+++ QtSocketTest/QtSocketTest.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <cstring>
+#include <string>
+
+#define SOCKET_PATH "tmp/socket"
+
+int main(){
+    int qtSocket;
+    struct sockaddr_un addr;
+    bool status = true;
+
+    qtSocket = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (qtSocket < 0){
+        perror("Socket Error");
+        status = false;
+        return 1;
+    }
+
+    addr.sun_family = AF_UNIX;
+    std::strcpy(addr.sun_path, SOCKET_PATH);
+    if (connect(qtSocket,(struct sockaddr*)&addr, sizeof(addr) - 1) == -1){
+        perror("Connection Error");
+        status = false;
+        return 1;
+    }
+
+    
+    while(status){
+        //Code for reciving data
+    }
+
+    unlink(SOCKET_PATH);
+    return 0;
+
+}
