531ac47cdf9f0dc54041007e98f387a34b582c89
DidBer <didrikber@gmail.com>
2025-05-08 14:49:33 +0200

    Update QtSocketTest.cpp

--- QtSocketTest/QtSocketTest.cpp
+++ QtSocketTest/QtSocketTest.cpp
@@ -2,10 +2,11 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
-#include <cstring>
+#include <string>
 #include <nlohmann/json.hpp>
 
 #define SOCKET_PATH "tmp/socket"
+
 
 int main(){
     int qtSocket;
@@ -23,30 +24,33 @@ int main(){
     std::strcpy(addr.sun_path, SOCKET_PATH);
     if (connect(qtSocket,(struct sockaddr*)&addr, sizeof(addr) - 1) == -1){
         perror("Connection Error");
+        close(qtSocket);
         status = false;
         return 1;
     }
 
     char buffer[1024];
-    ssize_t bytesRead = recv(qtSocket, buffer, sizeof(buffer) - 1, 0);
-    if (bytesRead > 0){
-        buffer[byttesRead] = '\0';
-        try {
-            nlohmann::json jsonData = nlohmann::json::parse(buffer);
-            std::cout << "Received: " << jsonData.dump(4) << std::endl;
-        } catch (const nlohmann::json::parse_error& e) {
-            std::cerr << "JSON Parse Error: " << e.what() << std::endl;
-        }
-    }
-    else if {bytesRead == 0){
-        std::cout << "Connection closed by server." << std::endl;
-    }
-    else {
-        perror("Receive Error");
-    }
+    std::string rest;
+    while(status){
+        ssize_t dataRecived = recv(qtSocket, buffer, sizeof(buffer), 0);
+        if (dataRecived <= 0) break;
+        rest.append(buffer, dataRecived);
 
+        size_t pos;
+        while(pos = rest.find('\n') != std::string::npos){
+            std::string data = rest.substr(0, pos);
+            rest.erase(0, pos + 1);
+            try { 
+                nlohmann::json jsonData = nlohmann::json::parse(data);
+                std::cout << "Received Data: " << jsonData.dump(4) << std::endl;
+            }   catch (const std::exception& e) {
+                std::cerr << n"Error parsing JSON: " << e.what() << std::endl;
+            }
+        
+        }
+
+    }
     close(qtSocket);
-    unlink(SOCKET_PATH);
     return 0;
 
 }
