dce1b2378dfab9099dd900d2a6d46804e5061120
DidBer <didrikber@gmail.com>
2025-05-11 12:01:18 +0200

    Update startupShell.sh

--- startupShell.sh
+++ startupShell.sh
@@ -1,6 +1,10 @@
 #!bin/bash
 
+#Start influx evironment
 source /home/kaffeknekt/influx_env/bin/activate
 
+#Start python script that controls sensor data to influxdb and socket
 python /home/kaffeknekt/kaffeknekt/temp_and_watt.py
 
+#Start QT app
+#/home/kaffeknekt/kaffeknekt/qtApp
