3fb6a663226b7c9d5eaf6d71eb3ec28a4d363356
DrBoonz <abdulkadirkabuk44@gmail.com>
2025-05-12 11:39:29 +0200

    Update influxsocketesp_trinity.py

--- influxsocketesp_trinity.py
+++ influxsocketesp_trinity.py
@@ -24,20 +24,23 @@ def main():
     clk = 0 #counter to add milliseconds to every other datawrite to avoid duplicate timestamps
     session = 1
 
-
+    #Define socket path
     path = '/tmp/socket'
-    
+
     if os.path.exists(path):
         os.unlink(path)
-        
-
-    server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    
+    server = socket.socket(
+        socket.AF_UNIX, #
+        socket.SOCK_STREAM
+    )
     
     server.bind(path)
     server.listen(1)
     
     
     try:
+        #Opens port to esp32 temporarily
         with serial.Serial(serial_port, baud_rate, timeout=1) as ser:
             print("Esp connected")
             
@@ -46,9 +49,11 @@ def main():
             print("Connected:", cli_addr)
             
             while True:
-                line = ser.readline().decode('utf-8').strip() #reads line, converts to string, and cleans excess characters
+                line = ser.readline().decode('utf-8').strip() #reads line, converts byte to string, and cleans excess characters like \n
+                
                 if line: #checks if line exists
                     try:
+                        #Convert json string to python dictionary
                         data = json.loads(line)
                         print(data)
 
@@ -68,6 +73,7 @@ def main():
                         elif data["flag"] == "D":
                             session = 0
 
+                        #Dropped condition to add tags for session activity
                         #if session == 1:
                         #    point = point.tag("sessionStatus", active)
                         #else:
@@ -83,7 +89,8 @@ def main():
                         #        point = point.field("flag", str("U"))
                         #    elif session == 0:
                         #        point = point.field("flag", str("D"))
-                        
+
+                        #Another method of filling out flag values with get()
                         if session == 1:
                             point = point.field("flag", data.get('flag', 1))
                         else:
@@ -93,19 +100,23 @@ def main():
                         if clk == 0:
                             clk = 1
                         elif clk == 1:
-                            clk = 0
+                             clk = 0
                        
                         #adder dataen for hver verdi i rekken
                         for field, value in data.items():
                             if field != "timestamp" and field != "flag":
                                 point = point.field(field, float(value))
                                 
-                            #elif field == "flag":
-                                #point = point.field(field, str(value))
+                            elif field == "flag":
+                                point = point.field(field, str(value))
 
                             elif field == "timestamp":
-                                point = point.time(int(value + clk*5*1e8 - (7200 * 1e9))) #temporary corrected time for random_generator
-                        
+                                point = point.time(int(value 
+                                                    #temporary corrected time for random_generator
+                                                    #+ clk*5*1e8 - (7200 * 1e9) 
+                                                      ))
+
+                        #Old point building code with fixed columns
                         #point = Point("Esp32Metrics") \
                             #.field("temperature_C", data["temperature_C"]) \
                             #.field("power_mW", data["power_mW"]) \
@@ -116,11 +127,11 @@ def main():
                         API.write(bucket=BUCKET, org=ORG, record=point, write_precision='ns')
                         print("Data written to Influx")
                         
-                        
+                        #Adding readable time to the json string
                         data["readable_time"] = readable_time
-                        
+
+                        #Converting python dictionary back to json string and adding newline for the recieving code to detect
                         socketstring = json.dumps(data) + '\n'
-                        #socketstring = socketstring + '\n'
                         
                         print(socketstring)
                         
@@ -133,7 +144,7 @@ def main():
                         print(f"{e} is missing.")
                         
     except KeyboardInterrupt:
-        print("\nStopped by user.")
+        print("Stopped by user.")
 
     except serial.SerialException as e:
         print(f"Serial error: {e}")
