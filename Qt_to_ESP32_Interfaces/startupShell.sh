3e9201f7bb6b9c52a9cb31e00b8427d44b664a00
DidBer <didrikber@gmail.com>
2025-05-18 18:01:45 +0200

    Update startupShell.sh

--- Qt_to_ESP32_Interfaces/startupShell.sh
+++ Qt_to_ESP32_Interfaces/startupShell.sh
@@ -4,7 +4,7 @@
 source /home/kaffeknekt/influx_env/bin/activate
 
 #Start python script that controls sensor data to influxdb and socket
-python /home/kaffeknekt/kaffeknekt/temp_and_watt.py
+python /home/kaffeknekt/kaffeknekt/database/influxsocketesp_trinity.py
 
 #Start QT app
-#/home/kaffeknekt/kaffeknekt/qtApp
+/home/kaffeknekt/kaffeknekt/qtApp
